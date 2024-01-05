80ebd74c38bacaffd54c97abe7ac4cc2ba4e3453
Abel Borit <abelboritguitton@gmail.com>
2024-01-05 17:44:54 -0500

    [FEAT] ToolbarModule & TableModule in prime-ng.module.ts

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,6 +4,8 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 /* este módulo solo exportará módulos de PrimeNG y con esto ganamos legibilidad y ordenamiento en el código aunque también puede ser que si por ejemplo solo usemos botones e inputs entonces va a cargarse todo pero en alguna parte de la aplicación ya estará cargado este módulo y Angular lo guarda en memoria entonces lo que se puede hacer también es dividir los módulos de PrimeNG según cómo lo utilicemos, es decir, un módulo para los botones, otro módulo para los inputs, etc., o sino tener todos lo módulos en un solo archivo como en este caso */
 @NgModule({
@@ -13,6 +15,8 @@ import { PanelModule } from 'primeng/panel';
     CardModule,
     FieldsetModule,
     PanelModule,
+    ToolbarModule,
+    TableModule,
   ],
 })
 export class PrimeNgModule {}
