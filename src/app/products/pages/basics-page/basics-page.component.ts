69ebb3377631160fe552f6374af87cfc974ba4ae
Abel Borit <abelboritguitton@gmail.com>
2024-01-05 11:54:17 -0500

    [FORMAT] basics-page.component add comments

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -12,3 +12,12 @@ export class BasicsPageComponent {
 
   public currentDate: Date = new Date();
 }
+
+/*
+Los pipes puros e impuros son un concepto esencial en Angular y tienen un impacto significativo en el rendimiento de una aplicación.
+
+  - Los pipes puros se ejecutan solo cuando cambian los argumentos que reciben, lo que significa que la transformación solo se realiza cuando es necesario.
+  - Los pipes impuros se ejecutan cada vez que hay un cambio en la aplicación, lo que podría afectar el rendimiento si se utilizan incorrectamente.
+
+Un caso de uso común para la memorización de pipes es cuando se tienen datos que no cambian con frecuencia, como listas de usuarios o datos estáticos. En lugar de recalcular la transformación cada vez que se renderiza el componente, podemos emplear un pipe puro con memorización para almacenar en caché los resultados y evitar cálculos innecesarios.
+*/
